5090a0f1ffb2f208a781dbae2d93392a270379ba
JaredIng87 <jaredingersoll14@gmail.com>
2022-11-26 22:26:52 -0500

    index model

--- models/index.js
+++ models/index.js
@@ -5,12 +5,36 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE', //Cascade or Null?
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: {
+    model: ProductTag,
+    unique: false //Necessary?
+  },
+  // Define an alias for when data is retrieved
+//Necessary?
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false //Necessary?
+  },
+  // Define an alias for when data is retrieved
+//Necessary?
+});
 
 module.exports = {
   Product,
